7e7a6a25bb7c30ca91ea57fb18d908ad0fb98032
Paulo Sereno <sereno2403@gmail.com>
2026-03-23 20:01:57 +0000

    feat: export content from HomePage to SliderOverflowCard component

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -14,30 +14,7 @@ function HomePage() {
         <SearchBar />
 
         <SliderOverflow>
-          <div className="sliderCard">
-            <div className="upper">
-              <div className="upperLeft">
-                <h1 className="contentTitle">Rima</h1>
-                <span className="contentDetails">
-                  Drama, Horror, Mystery & Thriller
-                </span>
-                <span className="contentDetails">15, March, 2020</span>
-              </div>
-              <div className="upperRight">
-                <img className="ratingIcon" src="" alt="Rating icon" />
-                <span>4.7</span>
-                {/* rate value */}
-              </div>
-            </div>
-            <div className="bottom">
-              <div className="bottomLeft">
-                <span>1h 30min, Arabic</span>
-              </div>
-              <div className="bottomRight">
-                <button className="videoBtn">PLAY ICON</button>
-              </div>
-            </div>
-          </div>
+          
         </SliderOverflow>
 
         <section className="myList">
