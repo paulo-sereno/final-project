4e50f8f0c32f0093a9675b08885fc033d124ccb6
Paulo Sereno <sereno2403@gmail.com>
2026-03-23 19:26:27 +0000

    feat: replace search img with GlassIcon SVG

--- src/icons/GlassIcon.tsx
+++ src/icons/GlassIcon.tsx
@@ -1,5 +1,21 @@
 function GlassIcon() {
-  return <img className="glassIcon" src="" alt="Magnifying glass icon" />;
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="lucide lucide-search-icon lucide-search"
+    >
+      <path d="m21 21-4.34-4.34" />
+      <circle cx="11" cy="11" r="8" />
+    </svg>
+  );
 }
 
 export default GlassIcon;
