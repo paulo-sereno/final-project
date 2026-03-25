e2a3c4bf17aabdd89994d65f78c424be73ce603d
Paulo Sereno <sereno2403@gmail.com>
2026-03-25 20:30:25 +0000

    feat: add PlayIcon component with SVG rendering

---
+++ src/icons/PlayIcon.tsx
@@ -0,0 +1,25 @@
+function PlayIcon() {
+  return (
+    <svg
+      width="35"
+      height="35"
+      viewBox="0 0 35 35"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        width="35"
+        height="35"
+        rx="17.5"
+        fill="#FF0000"
+        fillOpacity="0.94"
+      />
+      <path
+        d="M13 23.6048C13 24.3829 13.8497 24.8629 14.5162 24.4612L25.5789 17.794C26.224 17.4052 26.224 16.4698 25.5789 16.081L14.5162 9.41377C13.8497 9.01209 13 9.49208 13 10.2702V23.6048Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
+
+export default PlayIcon;
