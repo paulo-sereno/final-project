5ff08e4a1c2b66ad29ffb9b58a38929008e22450
Paulo Sereno <sereno2403@gmail.com>
2026-03-26 22:31:30 +0000

    refactor: change interface and type names

---
+++ src/types/MediaItem.ts
@@ -0,0 +1,33 @@
+export interface IMediaItem {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface IMovie extends IMediaItem {
+  original_title: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+}
+
+export interface ITvSeries extends IMediaItem {
+  first_air_date: string;
+  name: string;
+  origin_country: string[];
+  original_name: string;
+}
+
+export type MediaItem = IMovie | ITvSeries;
+
+// type guards
+export default function isMovie(item: MediaItem): item is IMovie {
+  return "title" in item;
+}
