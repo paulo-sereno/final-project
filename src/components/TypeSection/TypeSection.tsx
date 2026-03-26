0ad4236946b0753d837245c68ac5c6967c598c6a
sereno2403 <sereno2403@gmail.com>
2026-03-26 14:59:27 +0000

    feat: implement popular movie listing in TypeSection component

--- src/components/TypeSection/TypeSection.tsx
+++ src/components/TypeSection/TypeSection.tsx
@@ -1,13 +1,20 @@
+import usePopularCategory from "../../hooks/usePopularMovies";
+import type { IPopularMovie } from "../../types/PopularCategory";
 import styles from "./typeSection.module.css";
 import TypeSectionCard from "./TypeSectionCard/TypeSectionCard";
 import TypeSectionHeader from "./TypeSectionHeader/TypeSectionHeader";
 
 function TypeSection() {
+  const { popularCategory } = usePopularCategory<IPopularMovie>({
+    category: "movie",
+  });
   return (
     <>
       <section className={styles.typeSection}>
         <TypeSectionHeader title={"Movies"} />
-        <TypeSectionCard />
+        {popularCategory.map((movie) => (
+          <TypeSectionCard key={movie.id} movie={movie} />
+        ))}
       </section>
     </>
   );
