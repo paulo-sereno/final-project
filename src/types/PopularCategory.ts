5c3341c5fcb7773d5f3898d0cbbf982339d9bdad
sereno2403 <sereno2403@gmail.com>
2026-03-26 11:34:39 +0000

    feat: add IPopularBase, IPopularMovie, IPopularTV interfaces and IPopularCategory type

---
+++ src/types/PopularCategory.ts
@@ -0,0 +1,28 @@
+interface IPopularBase {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface IPopularMovie extends IPopularBase {
+  original_title: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+}
+
+export interface IPopularTV extends IPopularBase {
+  first_air_date: string;
+  name: string;
+  origin_country: string[];
+  original_name: string;
+}
+
+export type IPopularCategory = IPopularMovie | IPopularTV;
