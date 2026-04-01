47f6919cd8d3855a5bcf98f458f9117fd69c56f5
Paulo Sereno <sereno2403@gmail.com>
2026-04-01 23:29:25 +0100

    feat: update routing paths in main.tsx for MoviesPage, TvPage, and DetailsPage

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import MoviesPage from "./pages/MoviesPage";
 import TvPage from "./pages/TvPage";
+import DetailsPage from "./pages/DetailsPage";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route index element={<HomePage />} />
-        <Route path="movies" element={<MoviesPage />} />
-        <Route path="tvseries" element={<TvPage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/tvseries" element={<TvPage />} />
+        <Route path="/details/:type/:id" element={<DetailsPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
