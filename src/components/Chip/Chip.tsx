eccb6cc63dec322bcf5cc68dc6342885270fef28
Paulo Sereno <sereno2403@gmail.com>
2026-04-01 23:29:40 +0100

    feat: add hasIcon prop to Chip component for optional icon display

--- src/components/Chip/Chip.tsx
+++ src/components/Chip/Chip.tsx
@@ -1,8 +1,18 @@
+import RatingIcon from "../../icons/RatingIcon";
 import styles from "./chip.module.css";
 
-function Chip({ label, variant,}: { label: string; variant: "primary" | "secondary" | "terciary";}) {
+function Chip({
+  label,
+  variant,
+  hasIcon = false,
+}: {
+  label: string;
+  variant: "primary" | "secondary" | "terciary";
+  hasIcon?: boolean;
+}) {
   return (
     <div className={`${styles.chip} ${styles[variant]}`}>
+      {hasIcon && <RatingIcon type={"full"} />}
       <span>{label}</span>
     </div>
   );
