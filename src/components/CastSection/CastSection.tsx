214955ba017aa9f234cc52dd312920f3f3f40da2
Paulo Sereno <sereno2403@gmail.com>
2026-04-05 01:16:41 +0100

    feat: add styling for CastSection component with CSS module

--- src/components/CastSection/CastSection.tsx
+++ src/components/CastSection/CastSection.tsx
@@ -1,4 +1,5 @@
 import type { ICast } from "../../types/Details";
+import styles from "./castSection.module.css";
 
 interface ICastSection {
   cast: ICast[];
@@ -6,16 +7,17 @@ interface ICastSection {
 
 function CastSection({ cast }: ICastSection) {
   return (
-    <div>
-      <div>Actors</div>
-      <div>
+    <div className={styles.castSection}>
+      <div className={styles.title}>Actors</div>
+      <div className={styles.castList}>
         {cast.map((actor) => (
-          <div key={actor.id}>
+          <div key={actor.id} className={styles.actorCard}>
             <img
+              className={styles.actorImage}
               src={`https://image.tmdb.org/t/p/w200${actor.profile_path}`}
               alt={`${actor.name} image`}
             />
-            <span>{actor.name}</span>
+            <span className={styles.actorName}>{actor.name}</span>
           </div>
         ))}
       </div>
